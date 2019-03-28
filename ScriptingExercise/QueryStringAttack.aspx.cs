5f9ad908ebc9d10ba46bbe2e782e93c5bffa3b22
aleksandra.a.gaj <aleksandra.a.gaj.10@aberdeen.ac.uk>
2019-03-28 19:10:08 +0000

    Add project files.

---
+++ ScriptingExercise/QueryStringAttack.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ScriptingExercise
+{
+    public partial class QueryStringAttack : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string id = Request.QueryString["id"];
+            if(id==null)
+            {
+                lblOutput.Text = "NA";
+            }
+            else
+            {
+                lblOutput.Text = id;
+            }
+        }
+    }
+}
